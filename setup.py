578c5433e846da49a89b8719700cf436d288a7f0
Nick M <nickmaccarthy@gmail.com>
2016-01-26 17:37:15 -0500

    v1.1 update

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0',
+    version='1.1',
 
     description='A python module to emulate the date math used in SOLR and Elasticsearch',
     long_description=long_description,
