939ecbe8ee2d5da40255bdea19c3e6eb7815ec53
Nick M <nickmaccarthy@gmail.com>
2016-01-27 11:27:55 -0500

    added datemath helper to return datetime objects instead of arrow objects

--- datemath/__init__.py
+++ datemath/__init__.py
@@ -1,7 +1,7 @@
 from .helpers import parse
 
-def dm(expr):
-    return parse(expr)
+def dm(expr, **kwargs):
+    return parse(expr, **kwargs)
 
-def datemath(expr):
-    return parse(expr)
+def datemath(expr, **kwargs):
+    return parse(expr, **kwargs).datetime
