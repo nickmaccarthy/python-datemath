b03b20fe67d9599f59c2f9d5389efd51c78cc6c6
Nick M <nickmaccarthy@gmail.com>
2016-03-30 09:26:59 -0400

    v1.4.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '1.3'
+version = '1.4.2'
 
 setup(
     name='python-datemath',
