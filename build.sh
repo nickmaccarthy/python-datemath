63ee955108590587f02366479b2ee9420c0971d6
Nick M <nickmaccarthy@gmail.com>
2019-10-27 23:27:53 -0400

    python version seperation

--- build.sh
+++ build.sh
@@ -3,17 +3,18 @@
 # builds our packages for pypi
 
 source env/bin/activate
-pip install -r requirements.txt
+pip2.7 install -r requirements-2.txt
+pip3.7 install -r requirements-3.txt
 
 echo "Removing any previous build dir..."
 rm -rf build
 echo "Removing any previous dist dir..."
 rm -rf dist
 
-echo "Building for python 2.x..."
-python setup.py sdist bdist_wheel
-echo "Building for python 3.x..."
-/usr/local/bin/python3.5 setup.py sdist bdist_wheel
+echo "Building for python 2.7..."
+python2.7 setup.py sdist bdist_wheel
+echo "Building for python 3.7+..."
+python3.7 setup.py sdist bdist_wheel
 
 echo "Running twine upload..."
 twine upload dist/*
