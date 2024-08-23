013d0bafe335ebfe057105041c12e9688c3905c8
Nick MacCarthy <nick.maccarthy@rialtic.io>
2024-08-23 08:00:16 -0400

    fix: match licence

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author_email='nickmaccarthy@gmail.com',
 
     # Choose your license
-    license='MIT',
+    license='Apache-2.0',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -55,7 +55,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'Apache-2.0',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
