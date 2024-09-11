8ae9eca396d8c427be06cc369739a30b67bca433
Nick MacCarthy <nick.maccarthy@rialtic.io>
2024-09-11 12:14:59 -0400

    chore: cleanup

--- datemath/helpers.py
+++ datemath/helpers.py
@@ -1,43 +1,3 @@
-'''
-A basic utility module for parsing math like strings relating to dates
-
-This is inspired by Date Math features in elasticsearch and aims to replicate the same functionality for python.
-
-DateMath (datemath or dm) suppor addition, subtraction and rounding at various granularities of "units" (a map of units to their shorthand is below for reference).
-Expressions can be chanied together and are read left to right.  '+' and '-' denote addition and subtraction while '/' denotes 'round', in this case is a 'round down' or floor.
-Round requires a unit (/d), while addition and subtraction require an integer value and a unit (+1d).  Whitespace is not allowed in the expression.  Absolute datetimes with datemath 
-can be made as well, with the datetime and datemath expressions delinated by '||' - example '2015-01-01||+1d' == '2015-01-02'
-
-
-Maps:
-
-y or Y      =   'year'
-M           =   'month'
-m           =   'minute'
-d or D      =   'day'
-w           =   'week'
-h or H      =   'hour'
-s or S      =   'second'
-
-Examples:
-
-Assuming our datetime is currently: '2016-01-01T00:00:00-00:00'
-
-Expression:                 Result:
-now-1h                      2015-12-31T23:00:00+00:00
-now-1y                      2015-01-01T00:00:00+00:00
-now+1y+2d                   2017-01-03T00:00:00+00:00
-now+12h                     2016-01-01T12:00:00+00:00
-now+1d/d                    2016-01-03T00:00:00+00:00
-+2h                         2016-01-01T02:00:00+00:00
-+1h/h                       2016-01-01T02:00:00+00:00
-now+1w/w                    2016-01-11T00:00:00+00:00
-now/d+7d+12h                2016-01-08T12:00:00+00:00
-2016-01-01||+1d             2016-01-02T00:00:00+00:00
-2015-01-01||+2w             2015-01-15T00:00:00+00:00
-
-'''
-
 from __future__ import annotations
 
 import os
@@ -249,8 +209,6 @@ def evaluate(expression: str, now: Arrow, timeZone: str = 'UTC', roundDown: bool
     if debug: print("evaluate() - Finished: {0}".format(now))
     if debug: print('\n\n')
     return now
-
-
 
 if __name__ == "__main__":
     if debug: print('NOW: {0}'.format(arrow.utcnow()))
