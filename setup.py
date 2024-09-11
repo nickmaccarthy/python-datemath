b39305b40f42cb9cf7b78d659050124e73e3f76b
Nick MacCarthy <nick.maccarthy@rialtic.io>
2024-09-11 12:15:24 -0400

    feat: move pypi setup from setup.py to setup.cfg, cleanup

--- setup.py
+++ setup.py
@@ -28,48 +28,9 @@ setup(
     version=version,
     download_url = 'https://github.com/nickmaccarthy/python-datemath/tarball/{0}'.format(version),
 
-    description='A python module to emulate the date math used in SOLR and Elasticsearch',
-
-    long_description_content_type="text/markdown",
-    long_description=long_description_from_readme,
-
-
     # The project's main homepage.
     url='https://github.com/nickmaccarthy/python-datemath',
 
-
-    # Author details
-    author='Nick MacCarthy',
-    author_email='nickmaccarthy@gmail.com',
-
-    # Choose your license
-    license='Apache-2.0',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'Apache-2.0',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
-
-    # What does your project relate to?
-    keywords='date math datemath elaticsearch solr',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -78,9 +39,6 @@ setup(
     package_data={'': ['*']},
     include_package_data=True,
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
